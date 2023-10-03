548fcc689cef868e85c63d9f1692afcbb43233dc
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2023-10-02 22:12:28 -0700

    Added death tracking

--- scenes/game.py
+++ scenes/game.py
@@ -1,10 +1,12 @@
 # Main Game Scene
 # Entirely written by humans with assistance from Copilot (75% human, 25% AI)
 import math
+import random
 import pygame
 import globals
 from utils.camera import Camera
 from utils.platform import Platform
+from utils.platform import platform_templates
 from utils.player import Player
 
 from utils.scene import SceneBase
@@ -15,7 +17,16 @@ class GameScene(SceneBase):
         self.camera = Camera()
         self.player = Player()
         self.angle = 0
-        self.platforms = [Platform(0, 100, Platform.TYPE_GRASS, platform_width_type=Platform.WIDTH_FLOOR, platform_x_velocity=0)]
+        self.platforms = [
+            Platform(0, 100, Platform.TYPE_GRASS, platform_width_type=Platform.WIDTH_FLOOR, platform_x_velocity=0),
+            Platform(0, 400, Platform.TYPE_GRASS, platform_width_type=Platform.WIDTH_THICK, platform_x_velocity=0),
+            Platform(560, 400, Platform.TYPE_GRASS, platform_width_type=Platform.WIDTH_THICK, platform_x_velocity=0),
+            Platform(280, 700, Platform.TYPE_GRASS, platform_width_type=Platform.WIDTH_THICK, platform_x_velocity=0),
+            Platform(0, 1000, Platform.TYPE_ICE, platform_width_type=Platform.WIDTH_THICK, platform_x_velocity=0),
+            Platform(280, 1000, Platform.TYPE_SPIKE, platform_width_type=Platform.WIDTH_THIN, platform_x_velocity=0),
+            Platform(560, 1000, Platform.TYPE_ICE, platform_width_type=Platform.WIDTH_THICK, platform_x_velocity=0),
+
+            ]
 
     def update(self, dt):
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -32,7 +43,39 @@ class GameScene(SceneBase):
         # Update player
         self.player.update(dt, self.platforms)
 
-        pass
+        # Update Camera
+        self.camera.move_to(self.player.y - (Player.CHARACTER_HEIGHT + globals.SCREEN_HEIGHT / 2))
+
+        # Remove any platforms that are off screen from list
+        heights_removed = []
+        for i in self.platforms:
+            print(i.y, self.camera.y)
+            if i.y < self.camera.y:
+                if i.y not in heights_removed:
+                    heights_removed.append(i.y)
+                self.platforms.remove(i)
+        
+        # Randomly add platforms from platform_templates (list of list of templates), for each height removed. first height is 300 above highest height remaining, second height is 300 above that, etc.
+        greatest_height = 0
+        for i in self.platforms:
+            if i.y > greatest_height:
+                greatest_height = i.y
+    
+        for i in range(0, len(heights_removed)):
+            plat_template = random.choice(platform_templates)
+            for j in plat_template:
+                j.y = greatest_height + ((i+1) * 300)
+                if j.type == Platform.TYPE_GRASS:
+                    j.type = random.choice([Platform.TYPE_GRASS, Platform.TYPE_ICE])
+                self.platforms.append(j)
+        
+        # Update platforms
+        for i in self.platforms:
+            i.update(dt)
+        
+        # Check if player is dead
+        if self.player.y < self.camera.y:
+            globals.scene_manager.switch_scene("title")
 
     def render(self):
         # Clear screen
